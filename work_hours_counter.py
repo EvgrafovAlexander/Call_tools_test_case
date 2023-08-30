e2198388711940a2f4eff732f1752939cb48798e
EvgrafovAlexander <nemesis92@me.com>
2023-08-30 21:46:55 +0500

    feat(test task): Добавить решение тестовых заданий
    Добавлен main
    
    Решено: #000001

--- work_hours_counter.py
+++ work_hours_counter.py
@@ -1,4 +1,6 @@
 # stdlib
+import argparse
+import logging
 from collections import defaultdict
 from pathlib import Path
 
@@ -13,8 +15,20 @@ def print_working_hours_stat(path_to_file: Path) -> None:
     hours_counter = defaultdict(list)
     with open(path_to_file, encoding='utf-8') as f:
         for line in f:
-            name, work_hours = line.rsplit(" ", 1)
-            hours_counter[name].append(int(work_hours))
+            try:
+                name, work_hours = line.rsplit(" ", 1)
+                hours_counter[name].append(int(work_hours))
+            except ValueError as e:
+                logging.error("Запись '{}' не распознана, описание ошибки: {}".format(line, e))
 
         for name, list_of_hours in hours_counter.items():
             print("{}: {}; sum: {}".format(name, ", ".join(map(str, list_of_hours)), sum(list_of_hours)))
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-p", "--path_to_file", type=str, help="Path to file with data, format: 'name number_of_hours'")
+    args = parser.parse_args()
+
+    path_to_file = Path(args.path_to_file)
+    print_working_hours_stat(path_to_file)
